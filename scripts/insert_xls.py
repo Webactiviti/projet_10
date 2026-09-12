0f2cd6e9897a3be9852f4a2ef1ca78754c1cb6f3
Webactiviti <contact@webactiviti.com>
2026-09-12 18:21:55 +0200

    Fix: mise à jour

---
+++ scripts/insert_xls.py
@@ -0,0 +1,197 @@
+import duckdb
+import pandas as pd
+import numpy as np
+from scipy import stats
+import os
+import sys
+from pathlib import Path
+
+print("--- DEBUT DU PIPELINE INSERTION ---\n")
+
+# répertoire des fichiers
+BASE_DIR = Path(__file__).resolve().parent.parent
+XLS_DIR = BASE_DIR /  "data/xls"
+PROCESSED_DIR = BASE_DIR / "data/processed"
+os.makedirs(PROCESSED_DIR, exist_ok=True)
+
+# Lecture dichier ERP
+df_erp_xls = None
+try:
+    df_erp_xls = pd.read_excel(f"{XLS_DIR}/Fichier_erp.xlsx",sheet_name="Sheet1")
+    print("Fichier ERP lu avec succès !")
+
+except FileNotFoundError:
+    print("Erreur : Le fichier ERP est introuvable. Vérifiez le chemin ou le nom.")
+    sys.exit(1)
+
+except ValueError as e:
+    # Capturé si le format n'est pas supporté ou le fichier n'est pas un vrai Excel
+    print(f"Erreur de format/valeur fichier erp : {e}")
+    sys.exit(1)
+
+except Exception as e:
+    # Attrape les autres erreurs (fichier corrompu, manque d'une dépendance comme openpyxl/xlrd...)
+    print(f"Une erreur inattendue est survenue sur fichier erp : {e}")
+    sys.exit(1)
+
+
+# lecture fichier liaison
+df_liaison_xls = None
+try:
+    df_liaison_xls = pd.read_excel(f"{XLS_DIR}/fichier_liaison.xlsx",sheet_name="Sheet1")
+    print("Fichier liaison lu avec succès !")
+
+
+except FileNotFoundError:
+    print("Erreur : Le fichier liaison est introuvable. Vérifiez le chemin ou le nom.")
+    sys.exit(1)
+
+except ValueError as e:
+    # Capturé si le format n'est pas supporté ou le fichier n'est pas un vrai Excel
+    print(f"Erreur de format/valeur fichier liaison: {e}")
+    sys.exit(1)
+
+except Exception as e:
+    # Attrape les autres erreurs (fichier corrompu, manque d'une dépendance comme openpyxl/xlrd...)
+    print(f"Une erreur inattendue est survenue fichier liaison : {e}")
+    sys.exit(1)
+
+
+# Lecture fichier web
+df_web_xls =None
+try:
+    df_web_xls = pd.read_excel(f"{XLS_DIR}/Fichier_web.xlsx",sheet_name="Sheet1")
+    print("Fichier web lu avec succès !")
+
+
+except FileNotFoundError:
+    print("Erreur : Le fichier web est introuvable. Vérifiez le chemin ou le nom.")
+    sys.exit(1)
+
+except ValueError as e:
+    # Capturé si le format n'est pas supporté ou le fichier n'est pas un vrai Excel
+    print(f"Erreur de format/valeur fichier web : {e}")
+    sys.exit(1)
+
+except Exception as e:
+    # Attrape les autres erreurs (fichier corrompu, manque d'une dépendance comme openpyxl/xlrd...)
+    print(f"Une erreur inattendue est survenue fichier web : {e}")
+    sys.exit(1)
+
+# Nombre de ligne de chaque fichier
+
+nb_ligne_erp = len (df_erp_xls)
+nb_ligne_liaison = len (df_liaison_xls)
+nb_ligne_web = len (df_web_xls)
+print ("\n-----------------------------------------------------------------------") 
+print ("Nombre de ligne des différents fichiers XLS")
+print (f"Nombre de ligne erp : {nb_ligne_erp} - Nombre de ligne liaison : {nb_ligne_liaison} - Nombre de ligne web : {nb_ligne_web}")
+print ("----------------------------------------------------------------------") 
+
+# ------------------------------------------------------------------------------------------
+# nettoyage des data frames ( supression des id Nan  , renommage colonne  et choix product)
+# ------------------------------------------------------------------------------------------
+
+df_erp_xls_clean = df_erp_xls.dropna (subset= ['product_id'] )
+
+df_liaison_xls["id_web"] =pd.to_numeric(df_liaison_xls["id_web"] , errors ='coerce').astype('Int64') # changement du type de la colonne
+df_liaison_xls_clean = df_liaison_xls.dropna (subset= ['product_id'] )
+
+
+df_liaison_xls_clean = df_liaison_xls_clean.dropna (subset= ['id_web'] )
+df_liaison_xls_clean = df_liaison_xls_clean.dropna (subset= ['product_id'] )
+
+df_web_xls["sku"] =pd.to_numeric(df_web_xls["sku"] , errors ='coerce').astype('Int64') # changement du type de la colonne
+df_web_xls_clean = df_web_xls.dropna(subset=["sku"])
+
+df_web_xls_clean = df_web_xls_clean.rename ( columns={ "sku" :"id_web"} ) # renommage pour être conforme au nom des colonnes 
+df_web_xls_clean = df_web_xls_clean[df_web_xls_clean['post_type'] == 'product'].copy()
+
+# nombre de ligne sans NaN
+
+nb_ligne_erp_clean = len (df_erp_xls_clean)
+nb_ligne_liaison_clean = len (df_liaison_xls_clean)
+nb_ligne_web_clean= len (df_web_xls_clean)
+print ("\n-----------------------------------------------------------------------") 
+print ("Nombre de ligne sans NaN après suppression")
+print (f"Nombre de ligne erp : {nb_ligne_erp_clean} - Nombre de ligne liaison : {nb_ligne_liaison_clean} - Nombre de ligne web : {nb_ligne_web_clean}")
+print ("----------------------------------------------------------------------") 
+# ---------------------------------
+# nombre de doublon par data frame
+# ---------------------------------
+
+nb_doublon_erp = df_erp_xls_clean['product_id'].duplicated().sum() # doublon sur la colonne product_id
+nb_doublon_liaison = df_liaison_xls_clean[['product_id','id_web']].duplicated().sum() # doublon sur le couple product_id et id_web
+nb_doublon_web = df_web_xls_clean['id_web'].duplicated().sum() # doublon sur la colonne id_web ( anciennement sku )
+print ("\n-----------------------------------------------------------------------") 
+print ("Nombre de doublon dans les data frames")
+print (f"product_id du fichier erp : {nb_doublon_erp} - product_id / id_web du fichier liaison : {nb_doublon_liaison} - id_web du fichier web : {nb_doublon_web} " )
+print ("----------------------------------------------------------------------") 
+
+# ---------------------------------
+# insertion dans la base duckDB
+# ---------------------------------
+db_path =BASE_DIR / "data/processed/fichiersql.duckdb"
+
+con = None
+try: 
+    con = duckdb.connect(db_path)
+    # Enregistrement des DataFrames dans DuckDB
+    con.register('erp_temp', df_erp_xls_clean)
+    con.execute ("CREATE or REPLACE TABLE erp as select * from erp_temp" )
+    
+    con.register('liaison_temp', df_liaison_xls_clean)
+    con.execute ("CREATE or REPLACE TABLE liaison as select * from liaison_temp" )
+    
+    con.register('web_temp', df_web_xls_clean)
+    con.execute ("CREATE or REPLACE TABLE web as select * from web_temp" )
+
+    con.execute ("CHECKPOINT")
+    print ("\nInsertion des fichiers terminé")
+    
+    
+except AssertionError as e:
+    print (f"Erreur echec du script: {e}", file=sys.stderr)
+    sys.exit(1)
+except Exception as e :
+    print (f"Erreur echec du script: {e}", file=sys.stderr)
+    sys.exit(1)
+finally :
+    if con is not None :
+        con.close()
+        print("Close connexion insert SQL ")
+
+# ---------------------------------
+# Affichage du nombre de ligne SQL
+# ---------------------------------
+con = None
+try: 
+    con = duckdb.connect(db_path)
+    
+    query_erp ="select count(*) from erp"
+    df_result_erp = con.execute(query_erp).df().iloc[0,0]
+
+    query_liaison ="select count(*) from liaison"
+    df_result_liaison = con.execute(query_liaison).df().iloc[0,0]
+
+    query_web ="select count(*) from web"
+    df_result_web = con.execute(query_web).df().iloc[0,0]
+    print ("\n-----------------------------------------------------------------------") 
+    print ("Nombre de ligne de chaque table")
+    print (f"ERP : {df_result_erp} - Liaison : {df_result_liaison} - Web : {df_result_web}")
+    print ("----------------------------------------------------------------------") 
+except AssertionError as e:
+    print (f"Erreur echec req affichage: {e}", file=sys.stderr)
+    sys.exit(1)
+except Exception as e :
+    print (f"Erreur echec req affichage: {e}", file=sys.stderr)
+    sys.exit(1)
+finally :
+    if con is not None :
+        con.close()
+        print("Close connexion affichage  data SQL")
+
+
+
+
+
---
+++ scripts/resultat_req.py
@@ -0,0 +1,131 @@
+import duckdb
+import pandas as pd
+import numpy as np
+from scipy import stats
+import os
+import sys
+from datetime import datetime 
+from pathlib import Path
+
+con = None
+# répertoire des fichiers
+BASE_DIR = Path(__file__).resolve().parent.parent
+OUT_DIR = Path.cwd()
+
+output_dir = OUT_DIR / "data/exports"
+
+timestamp = datetime.now().strftime("%Y%m%d")
+db_path =BASE_DIR / "data/processed/fichiersql.duckdb"
+
+timestamp_rpt = datetime.now().strftime("%d/%m/%Y")
+print(f"--- GENERATION DU RAPPORT du {timestamp_rpt} ---")
+
+try: 
+    con = duckdb.connect(db_path)
+    
+    # =========================
+    # Génération du Rapport
+     # =========================
+    query_ca = """
+    SELECT 
+        e.product_id,
+        l.id_web,
+        w.post_title AS nom_produit,
+        e.price AS prix_unitaire,
+        COALESCE(w.total_sales, 0) AS ventes_totales,
+        ROUND(e.price * COALESCE(w.total_sales, 0), 2) AS chiffre_affaires,
+        e.stock_quantity AS stock
+    FROM erp e
+    INNER JOIN liaison l ON e.product_id = l.product_id
+    INNER JOIN web w ON l.id_web = w.id_web
+    ORDER BY chiffre_affaires DESC;
+    """
+    
+    df_ca = con.execute(query_ca).df()
+    file_ca = os.path.join(
+        output_dir, f"rapport_chiffre_affaires_{timestamp}.xlsx"
+    )
+    df_ca.to_excel(file_ca, index=False, engine="openpyxl")
+    print ("\n-----------------------------------------------------------------------") 
+    print(f"Fichier Excel CA généré : {file_ca}")
+
+    ca_total = df_ca['chiffre_affaires'].sum()
+    print(f"Chiffre d'Affaires Total  : {ca_total:,.2f} €")
+    print ("----------------------------------------------------------------------")
+
+
+
+    # -------------------------------------------------------------
+    # CALCUL DU Z-SCORE STATISTIQUE SUR L'ENSEMBLE DES PRIX
+    # -------------------------------------------------------------
+    # Z-Score = (prix - moyenne_prix) / ecart_type_prix
+    cte_zscore = """
+        WITH dataset AS (
+            SELECT 
+                e.product_id,
+                l.id_web,
+                w.post_title AS nom_produit,
+                e.price AS prix_unitaire,
+                COALESCE(w.total_sales, 0) AS ventes_totales,
+                ROUND(e.price * COALESCE(w.total_sales, 0), 2) AS chiffre_affaires,
+                e.stock_quantity AS stock,
+                -- Calcul de la moyenne et de l'écart-type sur le périmètre
+                AVG(e.price) OVER () AS avg_price,
+                STDDEV(e.price) OVER () AS std_price
+            FROM erp e
+            INNER JOIN liaison l ON e.product_id = l.product_id
+            INNER JOIN web w ON l.id_web = w.id_web
+        )
+        SELECT 
+            product_id,
+            id_web,
+            nom_produit,
+            prix_unitaire,
+            ROUND((prix_unitaire - avg_price) / std_price, 4) AS z_score,
+            ventes_totales,
+            chiffre_affaires,
+            stock
+        FROM dataset
+    """
+    # -------------------------------------------------------------
+    # EXTRACTION : Vins Premium (Z-Score > 2) (.csv)
+    # -------------------------------------------------------------
+    query_premium = f"""
+        {cte_zscore}
+        WHERE (prix_unitaire - avg_price) / std_price > 2
+        ORDER BY z_score DESC;
+    """
+    df_premium = con.execute(query_premium).df()    
+    fichier_premium = os.path.join( output_dir, f"vins_premium_{timestamp}.csv")
+    df_premium.to_csv(fichier_premium, index=False, encoding="utf-8")
+    print ("\n-----------------------------------------------------------------------") 
+    print(f"Fichier CSV vins premium (Z-Score > 2) généré : {fichier_premium}")  
+    print(f"Nombre de vins millésimes : {len(df_premium)}")  
+    print ("----------------------------------------------------------------------")
+    # -------------------------------------------------------------
+    #  EXTRACTION : Vins Ordinaires (Z-Score <= 2) (.csv)
+    # -------------------------------------------------------------
+    query_ordinaires = f"""
+        {cte_zscore}
+        WHERE (prix_unitaire - avg_price) / std_price <= 2
+        ORDER BY z_score DESC;
+    """
+    df_ordinaires = con.execute(query_ordinaires).df()
+    fichier_ordinaire = os.path.join(output_dir, f"vins_ordinaires_{timestamp}.csv")
+    df_ordinaires.to_csv(fichier_ordinaire, index=False, encoding="utf-8")
+    
+    print ("\n-----------------------------------------------------------------------") 
+    print(f"Fichier CSV Vins Ordinaires (Z-Score <= 2) généré  : {fichier_ordinaire}")
+    print ("----------------------------------------------------------------------")
+    
+    
+except AssertionError as e:
+    print (f"Erreur echec du script: {e}", file=sys.stderr)
+    sys.exit(1)   
+except Exception as e :
+    print (f"Erreur echec du script: {e}", file=sys.stderr)
+    sys.exit(1)    
+finally :
+    if con is not None :
+        con.close()
+        print("Close connexion SQL")
